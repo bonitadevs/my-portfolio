e536b93717eb5f3a516d4cad92c0ee3887dc2db2
bonitadevs <ddw29@#me.com>
2023-03-08 17:20:30 -0500

    new component Sidebar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
-import './App.scss'
+
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
-        </Route>
+        <Route path="/" element={<Layout />} />
       </Routes>
     </>
   )
